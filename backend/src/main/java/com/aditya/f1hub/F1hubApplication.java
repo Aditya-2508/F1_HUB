1d5279ac312eca41d641632b5cf2d2c9290911aa
Aditya Satpute <aadisatpute2004@gmail.com>
2026-07-05 15:03:59 +0530

    chore(project): initialize Spring Boot backend and database configuration

---
+++ backend/src/main/java/com/aditya/f1hub/F1hubApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.f1hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1hubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1hubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aditya/f1hub/F1hubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.f1hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1hubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
